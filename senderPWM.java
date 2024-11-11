a87e6f05502518cd4cf47e9e41cfe3de95cf24aa
Ribeiro <49697146+goncalo3377@users.noreply.github.com>
2024-11-11 17:50:17 +0000

    Update senderPWM.java
    atualizado para enviar mensagem e depois fica à espera de uma resposta do cliente

--- senderPWM.java
+++ senderPWM.java
@@ -1,19 +1,16 @@
-import java.util.Scanner;  
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class senderPWM {
+public class TCPSender {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); 
-        System.out.println("Coloca aqui o valor de PWM (0-255): ");
-        // Mensagem a ser enviada 
-        String message = scanner.nextLine();  
-
         try {
             // Define o endereço IP e a porta do destino
-            String tcpIP = "192.168.137.3";  // IP do ESP32 ou servidor de destino
-            int tcpPort = 12345;            // Porta do ESP32 ou servidor de destino
+            String tcpIP = "192.168.137.145";  // IP do ESP32 ou servidor de destino
+            int tcpPort = 12345;               // Porta do ESP32 ou servidor de destino
 
             // Cria o socket TCP e conecta ao endereço e porta especificados
             Socket socket = new Socket(tcpIP, tcpPort);
@@ -22,15 +19,23 @@ public class senderPWM {
             OutputStream outputStream = socket.getOutputStream();
             PrintWriter writer = new PrintWriter(outputStream, true);
 
-            
-            
-            //int message = 10;
+            // Cria o fluxo de entrada para receber a resposta
+            InputStream inputStream = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // Mensagem a ser enviada
+            String message = "Hello from PC!";
             writer.println(message);  // Envia a mensagem
 
             System.out.println("Mensagem enviada: " + message);
 
-            // Fecha o fluxo e o socket
+            // Aguarda e lê a resposta do servidor
+            String response = reader.readLine();
+            System.out.println("Resposta recebida: " + response);
+
+            // Fecha os fluxos e o socket
             writer.close();
+            reader.close();
             socket.close();
         } catch (Exception e) {
             e.printStackTrace();
